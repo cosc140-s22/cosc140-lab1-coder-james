5b8cdf80aed7b2474a9eb70769dafc9751e3191b
florenceluo <99026236+florenceluo@users.noreply.github.com>
2022-02-18 15:39:43 +0000

    first submission lab 1

--- problem1.py
+++ problem1.py
@@ -1,6 +1,15 @@
 #
-# Name:
-# Collaborator(s):
+# Name: Florence
+# Collaborator(s): Rachel, James
 #
 
+secs=int(input("How many seconds?"))
+totalsec=secs
+days=secs//86400
+secs-=days*86400
+hr=secs//3600
+secs-=hr*3600
+mins=secs//60
+secs-=mins*60
 
+print(str(totalsec)+ " seconds is equivalent to " + str(days)+ "day, "+ str(hr) + " hours, " +str(mins) +" minutes, and " +str(secs)+ " seconds. ")
--- problem2.py
+++ problem2.py
@@ -1,6 +1,19 @@
 #
-# Name:
-# Collaborator(s):
+# Name: Florence
+# Collaborator(s): Rachel, James
 #
 
+import math
 
+lat1 = math.radians(float(input("what is the latitude for location 1?")))
+lon1 = math.radians(float(input("what is the longitude for location 1?")))
+lat2 = math.radians(float(input("what is the latitude for location 2?")))
+lon2 = math.radians(float(input("what is the longitude for location 2?")))
+
+dlon = lon2 - lon1
+dlat = lat2 - lat1
+x = (math.sin(dlat/2))**2 + math.cos(lat1)*math.cos(lat2)*(math.sin(dlon/2))**2
+y= 2*math.atan2(math.sqrt(x), math.sqrt(1-x))
+z = 3956 *y
+
+print ("The distance between the two locations is" + str(z) +"miles.")
